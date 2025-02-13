3ae4e4ec9d6605429a77fc68a580178609a7e9e1
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-02-13 14:56:57 +0600

    update menu

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../assets/Components/SectionTitle/SectionTitle"
+
+const Payment = () => {
+  return (
+    <div>
+      <SectionTitle heading={'Payment'} subHeading={'Resevation'}></SectionTitle>
+      <div>
+        <h2 className="text-4xl">payment Gatway</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Payment
